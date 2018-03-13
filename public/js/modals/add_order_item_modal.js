4a73296d0c941ba1707cf48f7de799d209f3cc83
Prithu Parker <prithu.p@impos.com.au>
2018-03-13 17:10:26 +1100

    Add variant and option support when adding new items to existing orders

--- public/js/modals/add_order_item_modal.js
+++ public/js/modals/add_order_item_modal.js
@@ -5,9 +5,15 @@ angular
     ]);
 
 function AddOrderItemController($scope, $modalInstance, OrderHelper, order, locationId, Meerkat) {
-    console.log(order);
+
+    $scope.loadDefault = function() {
+        $scope.itemForm = JSON.parse(
+                '{ "posId": "", "quantity": 1, "unitPrice": 1000, "name": "Pepperoni Pizza", "type": "single", "options": [ { "posId": "", "name": "Toppings", "variants": [ { "posId": "", "name": "Pineapple", "price": "300" }, { "posId": "", "name": "Olives", "price": "200" } ] } ], "surcounts": [], "includedItems": [ { "name": "Fries", "options": [ { "posId": "", "name": "Sauce", "min": "0", "max": "1", "variants": [ { "posId": "", "name": "BBQ", "price": "250" } ] } ], "quantity": 1, "unitPrice": 0, "posId": "" } ], "totalBeforeSurcounts": "10002.50", "totalAfterSurcounts": "10002.500" }'
+            );
+    };
+
     $scope.itemForm = {
-        posId: "0",
+        posId: "",
         quantity: 1,
         unitPrice: 1000,
         name: "Pepperoni Pizza",
@@ -31,12 +37,58 @@ function AddOrderItemController($scope, $modalInstance, OrderHelper, order, loca
         $scope.itemForm.totalBeforeSurcounts = beforeSurcount;
         $scope.itemForm.totalAfterSurcounts = beforeSurcount + surcountCharge;
 
+        clearEmptyPosId();
+
+        if (typeof($scope.itemForm.includedItems) !== "undefined") {
+            $scope.itemForm.type = "bundle";
+        }
+
         Meerkat.addOrderItems(locationId, order, [$scope.itemForm]).then(
             response => {
                 $scope.$root.$broadcast("ordersUpdatedEvent", {});
                 $modalInstance.close();
             });
     };
+
+    function clearEmptyPosId() {
+        if ($scope.itemForm.posId.length === 0)
+            delete $scope.itemForm.posId;
+
+        if (typeof($scope.itemForm.includedItems) !== "undefined") {
+            for (var i = 0; i < $scope.itemForm.includedItems.length; ++i) {
+                var include = $scope.itemForm.includedItems[i];
+
+                clearPosId(include);
+
+                for (var j = 0; j < include.options.length; ++j) {
+                    var option = include.options[j];
+
+                    clearPosId(option);
+
+                    for (var k = 0; k < option.variants.length; ++k) {
+                        var variant = option.variants[k];
+
+                        clearPosId(variant);
+                    }
+                }
+            }
+        }
+
+        for (var i = 0; i < $scope.itemForm.options.length; ++i) {
+            var option = $scope.itemForm.options[i];
+            clearPosId(option);
+
+            for (var j = 0; j < option.variants.length; ++j) {
+                var variant = option.variants[j];
+                clearPosId(variant);
+            }
+        }
+    }
+
+    function clearPosId(obj) {
+        if (obj.posId.length === 0)
+            delete obj.posId;
+    }
 
     function calculateItemCost() {
         var beforeSurcount = $scope.itemForm.quantity * $scope.itemForm.unitPrice;
@@ -47,7 +99,7 @@ function AddOrderItemController($scope, $modalInstance, OrderHelper, order, loca
         for (var i = 0; i < $scope.itemForm.includedItems.length; ++i) {
             var include = $scope.itemForm.includedItems[i];
 
-            beforeSurcount += include.quantity * include.unitPrice;
+            beforeSurcount += Number(include.quantity) * Number(include.unitPrice);
             for (var j = 0; j < include.options.length; ++j) {
                 var option = include.options[j];
                 if (option.variants === null)
@@ -55,8 +107,18 @@ function AddOrderItemController($scope, $modalInstance, OrderHelper, order, loca
 
                 for (var k = 0; k < option.variants.length; ++k) {
                     var variant = option.variants[k];
-                    beforeSurcount += variant.price;
+                    beforeSurcount += Number(variant.price);
                 }
+            }
+        }
+
+        for (var i = 0; i < $scope.itemForm.options.length; ++i) {
+            var option = $scope.itemForm.options[i];
+            
+            for (var j = 0; j < option.variants.length; ++j) {
+                var variant = option.variants[j];
+
+                beforeSurcount += Number(variant.price);
             }
         }
 
@@ -77,18 +139,54 @@ function AddOrderItemController($scope, $modalInstance, OrderHelper, order, loca
     }
 
     $scope.newInclude = function() {
+        if (typeof($scope.itemForm.includedItems) === "undefined")
+            $scope.itemForm.includedItems = [];
+
         $scope.itemForm.includedItems.push({
             name: "",
             options: [],
             quantity: 1,
             unitPrice: 0,
-            posId: "0"
+            posId: ""
         });
     }
 
     $scope.deleteInclude = function(include) {
-        var index = $scope.itemForm.includedItems.indexOf(include);
-        if (index >= 0)
-            $scope.itemForm.includedItems.splice(index, 1);
+        $scope.removeItemFromArray($scope.itemForm.includedItems, include);
+
+        if ($scope.itemForm.includedItems.length == 0)
+            delete $scope.itemForm.includedItems;
     };
+
+    $scope.newOption = function(include) {
+        include.options.push({
+            posId: "",
+            name: "",
+            min: "0",
+            max: "1",
+            variants: []
+        });
+    };
+
+    $scope.newOptionNoLimits = function(include) {
+        include.options.push({
+            posId: "",
+            name: "",
+            variants: []
+        });
+    };
+
+    $scope.newOptionVariant = function(option) {
+        option.variants.push({
+            posId: "",
+            name: "",
+            price: 0
+        });
+    };
+
+    $scope.removeItemFromArray = function(array, item) {
+        var index = array.indexOf(item);
+        if (index >= 0)
+            array.splice(index, 1);
+    }
 }
